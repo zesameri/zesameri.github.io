b6dcb4c02b068a5b0778a202a4f1a273c7477fd1
merhoo <meredith.hoo1@gmail.com>
2016-01-11 23:05:34 -0500

    I messed something up with git and fixed it? but hopefully my last changes were also saved

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var rotation = 0;
+
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+
+$('.rotate').click(function() {
+    rotation += 5;
+    $(this).rotate(rotation);
+});
