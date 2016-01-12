fd4366ffc76e5e13ad636c22c8866898470ed376
merhoo <meredith.hoo1@gmail.com>
2016-01-12 16:50:20 -0500

    added site icon and small animation
    most of the icons are on the slim chance i change my mind
    able to access static svg path's ids and added jquery hover events to
    each petal to change the site title

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 
-/*var petals = $('#flower').children('svg').children('path');
-petals.each(function () {
-    console.log(this.id);
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+    }
+}, 10);
+console.log(readyStateCheckInterval);
+var petals = $('#flower').children('svg').children('path');
+
+petals.each(
+	function () {
+		$(this).hover(
+		function() {
+			$("#site-title").text(this.id);
+		}, 
+		function() {
+			$("#site-title").text("MEREDITH HOO");
+		})
 });
-*/
-var svg = $('#flower').svg({loadURL: './assets/flower.svg'});
-var paths = svg.children('path');
-paths.each(function () {
-    console.log(this.id);
-});
+
 /*
+petals.each(function () {
+	$( this ).mouseenter( 
+		function() {
+			$("#site-title").text(this.id);
+		}).mouseleave( 
+		function() {
+		$("#site-title").text("this.id");
+		})
+});
+
+
+Adding Animation
 var rotation = 0;
 
 jQuery.fn.rotate = function(degrees) {
@@ -24,4 +45,13 @@ $('.rotate').click(function() {
     rotation += 5;
     $(this).rotate(rotation);
 });
+
+Making the code work for an external svg
+var svg = $('#flower').svg('get');
+var paths = svg.children('path');
+paths.each(function () {
+    console.log(this.id);
+});
+
+
 */
