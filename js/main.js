846678729ef386bdc064f5fecf7a5341fd3f66d5
merhoo <meredith.hoo1@gmail.com>
2016-01-12 19:17:18 -0500

    hope this works

--- js/main.js
+++ js/main.js
@@ -1,61 +1,23 @@
 
-<<<<<<< HEAD
 var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
         clearInterval(readyStateCheckInterval);
     }
 }, 10);
-
-var svgdoc = null;
-var svgwin = null;
-var object = document.getElementById('flower');
-if (object && object.contentDocument)
-	svgdoc = object.contentDocument;
-else try {
-	svgdoc = object.getSVGDOcument();
-}
-catch (exception) {
-	console.log('Neither the HTMLObjectElement nor the GetSVGDocument interface are implemented');
-}
-if (svgdoc && svgdoc.defaultView)
-	svgwin = svgdoc.defaultView;
-else if (object.window)
-	svgwin = object.window;
-else try {
-	svgwin = object.getWindow();
-}
-catch(exception) {
-	console.log('The DocumentView interface is not supported\r\n' +
-      'Non-W3C methods of obtaining "window" also failed');
-}
-console.log(svgdoc.getElementsByTagName("path"));
-
-/*
-for (petal in petals) {
-	petal.hover(function() {
+var petals = $('#flower').children('svg').children('path');
+petals.each(function () {
+		$(this).hover(
+		function() {
 			$("#site-title").text(this.id);
 		}, 
 		function() {
 			$("#site-title").text("MEREDITH HOO");
-		});
-}
-
-
-
-Adding Animation
-=======
-/*var petals = $('#flower').children('svg').children('path');
-petals.each(function () {
-    console.log(this.id);
+		})
 });
-*/
-var svg = $('#flower').svg({loadURL: './assets/flower.svg'});
-var paths = svg.children('path');
-paths.each(function () {
-    console.log(this.id);
-});
+
+
 /*
->>>>>>> parent of fd4366f... added site icon and small animation
+Adding Animation
 var rotation = 0;
 
 jQuery.fn.rotate = function(degrees) {
