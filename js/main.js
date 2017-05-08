ebf488fa67161c174124cc19f7a71d5af10704ca
Meredith Hoo <meredith.hoo1@gmail.com>
2017-05-08 16:49:54 -0400

    small changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-	$( ".snip1168 li" ).first().addClass('current');
 	$(".goto").click(function() {
 		let link = $(this).attr('ref');
 		$('html,body').animate({
 			scrollTop: $(link).offset().top},
 			'slow');
 	});
-	$(".snip1168 li").click(function() {
-		$(".snip1168 li").removeClass('current');
-    $(this).addClass('current');
-	})
+	
 });
