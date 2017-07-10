1b35a8dd07187d36ee0b7fc960eeb739e5c6fa5f
Meredith Hoo <meredith.hoo1@gmail.com>
2017-07-10 00:22:11 -0700

    added menu animations

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
 			'slow');
 	});
 	$(".cell").click(function(){
-		$(this).children(":visible").fadeOut();
+		$(this).children(":visible").fadeOut(1);
 		$(this).children(":hidden").delay(500).fadeIn();
+		if ($(this).find(".back:visible")) {
+			$(this).siblings().fadeOut();
+			$(this).parent().siblings().fadeOut();
+		}
+		if ($(this).find(".front:visible")) {
+			$(".flex-row").delay(1000).show();
+			$(".cell").delay(1000).show();
+		}
 	});
 });
