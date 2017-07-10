fcffd5c0dfbe069f4c8661fedb5830f646b89314
Meredith Hoo <meredith.hoo1@gmail.com>
2017-07-09 23:41:54 -0700

    animated grid cells

--- js/main.js
+++ js/main.js
@@ -5,5 +5,8 @@ $(document).ready(function() {
 			scrollTop: $(link).offset().top},
 			'slow');
 	});
-	
+	$(".cell").click(function(){
+		$(this).children(":visible").fadeOut();
+		$(this).children(":hidden").delay(500).fadeIn();
+	});
 });
