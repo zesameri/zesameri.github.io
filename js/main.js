897c3e445522c89c9f1445d09edf3f5694d3fa53
Meredith Hoo <meredith.hoo1@gmail.com>
2017-07-23 11:22:40 -0700

    testing

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ $(document).ready(function() {
 			'slow');
 	});
 	$(".cell").click(function(){
-		$(this).children(":visible").fadeOut(1);
-		$(this).children(":hidden").delay(500).fadeIn();
-		if ($(this).find(".back:visible")) {
+		if ($(this).find(".front:visible")) {
+			$(this).children().fadeOut();
 			$(this).siblings().fadeOut();
 			$(this).parent().siblings().fadeOut();
+			$(this).children(":hidden").delay(500).fadeIn();
 		}
-		if ($(this).find(".front:visible")) {
+		if ($(this).find(".back:visible")) {
+			$(this).children(":visible").fadeOut(1);
+			$(this).children(":hidden").delay(500).fadeIn();
 			$(".flex-row").delay(1000).show();
 			$(".cell").delay(1000).show();
 		}
