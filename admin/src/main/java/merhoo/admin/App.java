543af45a11edc2195649060573a574d7ebf5c7c5
Bradley <mgh219@lehigh.edu>
2018-02-18 22:19:23 -0800

    added the skeleton of the admin for managing the backend with maven

---
+++ admin/src/main/java/merhoo/admin/App.java
@@ -0,0 +1,13 @@
+package merhoo.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
