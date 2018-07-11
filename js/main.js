632e928bd1ba2f8a9ac2fc7a1737c0409240e197
merhoo <meredith.hoo1@gmail.com>
2018-07-11 13:18:00 -0700

    changed color and responsive sizing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 $(window).scroll(function() {
   var isMobile = window.matchMedia("only screen and (max-width: 850px)").matches ||
                   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  if(isMobile) {
+  var isOtherMediaQuery = window.matchMedia("only screen and (min-width:1080px)").matches;
+  if(isMobile && !isOtherMediaQuery) {
    // some code..
    console.log('there');
  } else {
@@ -15,10 +16,11 @@ $(window).scroll(function() {
    if (wS > (hT+hH-wH)){
      $('#footer').addClass("menu-footer");
      $('#footer').removeClass("hero-footer");
-
+     console.log("welp");
    } else {
      $('#footer').addClass("hero-footer");
      $('#footer').removeClass("menu-footer");
+     console.log("welp there");
    }
  }
 });
