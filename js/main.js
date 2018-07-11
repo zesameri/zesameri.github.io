f3df32d6834cab9a16705cb02894dd18eb0ec75b
merhoo <meredith.hoo1@gmail.com>
2018-07-11 12:37:39 -0700

    removed net neutrality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-$( document ).ready(function() {
-console.log( "ready!" );
-  $(".btn-link").click(function() {
-    $(".net-neutral-header").hide();
-  })
+
+$(window).scroll(function() {
+  var isMobile = window.matchMedia("only screen and (max-width: 850px)").matches ||
+                  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  if(isMobile) {
+   // some code..
+   console.log('there');
+ } else {
+   console.log('here');
+   var hT = $('#menu').offset().top,
+       hH = $('#menu').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+
+   if (wS > (hT+hH-wH)){
+     $('#footer').addClass("menu-footer");
+     $('#footer').removeClass("hero-footer");
+
+   } else {
+     $('#footer').addClass("hero-footer");
+     $('#footer').removeClass("menu-footer");
+   }
+ }
 });
