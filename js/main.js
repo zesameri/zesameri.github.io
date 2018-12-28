7313acd5a148d2482127d1407943c04ebb3f7f0c
merhoo <meredith.hoo1@gmail.com>
2018-12-28 00:08:59 -0500

    updated hrefs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 /**
  * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2017, Codrops
- * http://www.codrops.com
  */
 {
 	class ImgItem {
