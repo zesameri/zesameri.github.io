60cc510c82100ea3517ec00c96e2331a847d9346
James Witte <jameswitte@Jamess-iMac.local>
2019-01-01 18:02:08 -0500

    starting grid work

--- js/secrets.js
+++ js/secrets.js
@@ -29,6 +29,44 @@ for (var k = 2; k < 44; k++) {
 }
 // two.add(group)
 
+// Setup the canvas
+var two = new Two({
+  fullscreen: true,
+    autostart: true
+}).appendTo(document.body);;
+
+var background = two.makeRectangle(two.width / 2, two.height / 2,
+  two.width, two.height);
+background.noStroke();
+background.fill = 'black';
+background.name = 'background';
+
+var container = two.makeGroup(background);
+
+var rows = Math.floor(two.height / 100);
+var cols = Math.floor(two.width / 100);
+var width = Math.round(two.height / Math.max(rows, cols));
+var height = width;
+
+for (var r = 0; r < rows; r++) {
+  // even rows have an offset of 0.5
+  var even = = !!(r % 2);
+  var vi = r / (rows - 1);
+  for (var c = 0; c < cols; c++) {
+    var k = c;
+    if (even) {
+      k += 0.5;
+      // we skip the final col on even rows to avoid overflow
+      if (c  <  cols -  1) {
+        var hi = k /(cols -  1);
+
+      }
+    }
+  }
+}
+
+two.update();
+
 function roseMath(v, k, t) {
   v.x = radius * Math.cos(k * t) * Math.cos(t);
   v.y = radius * Math.cos(k * t) * Math.sin(t);
