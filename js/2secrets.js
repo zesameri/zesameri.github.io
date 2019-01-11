340dbfe3ba443046e54b85ac4626aee276174f7c
merhoo <meredith.hoo1@gmail.com>
2019-01-11 14:29:46 -0500

    fixed animation/render issue part 1

--- js/2secrets.js
+++ js/2secrets.js
@@ -1,34 +1,33 @@
-// Setup the canvas
-var type = /(canvas|webgl)/.test(url.type) ? url.type : 'svg';
-var two = new Two({
-  type: Two.Types[type],
-  fullscreen: true,
-    autostart: true
-}).appendTo(document.body);
 document.body.style.background = 'black';
 
+var height = $('body').height();
+var width = $('body').width();
+var mobileSize = 200;
+var desktopSize = 150;
+var radius = 0, rows  = 0, cols = 0, size;
 
-var radius = 0, rows  = 0, cols = 0;
-var isMobile = window.matchMedia("only screen and (max-width: 850px)").matches ||
-                /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-var isOtherMediaQuery = window.matchMedia("only screen and (min-width:1080px)").matches;
-if(isMobile && !isOtherMediaQuery) {
-  rows = Math.floor(two.height / 200);
-  cols = Math.floor(two.width / 200);
-  radius = Math.floor(two.height / rows) * .62;
-} else {
-  rows = Math.floor(two.height / 150);
-  cols = Math.floor(two.width / 150);
-  radius = Math.floor(Math.max(two.width, two.height) / Math.max(rows, cols)) / 2;
-}
+// Define rows, colnums, and shape radius based on document size
+// var isMobile = window.matchMedia("only screen and (max-width: 850px)").matches ||
+//                 /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// var isOtherMediaQuery = window.matchMedia("only screen and (min-width:1080px)").matches;
+size = desktopSize;
+rows = Math.floor(height / size);
+cols = Math.floor(width / size);
+radius = Math.floor(Math.max(width, height) / Math.max(rows, cols)) / 2;
+
 var shapes = makeFlowers();
 var flowers = [];
 
 for (var r = 0; r < rows; r++) {
+  var rowId = "row" + r;
+  var row = $("<div/>").addClass("row").attr("id", rowId).appendTo('body');
   // even rows have an offset of 0.5
   var even = !!(r % 2);
   var vi = r / (rows - 1);
   for (var c = 0; c < cols; c++) {
+    // var cell = $("<div/>").addClass("cell").appendTo(row);
+    var cellId = "cell" + ((r * rows) + c);
+    $(row).append('<div class="cell" id="' + cellId + '"></div>')
     var k = c;
     if (even) {
       k += 0.5;
@@ -36,19 +35,33 @@ for (var r = 0; r < rows; r++) {
       if (c  >=  cols -  1) {
         continue;
       }
+    } else {
+      row.css("position", "relative");
+      row.css("left", (size / 2 * -1 + "px"));
     }
     var hi = k /(cols -  1);
     var shape = pickFlower(shapes);
-    shape.translation.set(hi * two.width, vi * two.height);
-    flowers.push(shape);
+    shape.translation.set((size + 25) / 2, (size + 25) / 2);
+    // shape.translation.set(hi * width, vi * height);
+    // Setup the canvas
+    var two = new Two({
+      width: size + 25,
+      height:size + 25
+    }).appendTo(document.getElementById(cellId));
+
+    $(cellId).click(function(e) {
+      
+    }
+
     two.add(shape);
+    two.update();
+    flowers.push(shape);
   }
 }
-two.update();
+
 
 for (var f in flowers) {
   var flower = flowers[f];
-  console.log(flower.id);
   $(flower._renderer.elem)
     .click(function(e) {
       flower.fill = "blue";
@@ -82,14 +95,14 @@ function pickFlower(flowers) {
   var f = Math.floor(Math.random() * flowers.length);
   var flower = flowers[f].clone();
   // Style the shape
-  var colors = ['tomato', 'lightsalmon', 'floralwhite', 'orangered',
+  var colors = ['tomato', 'orangered',
     'gold', 'red', 'darkorange'];
   var color = colors[Math.floor(Math.random() * colors.length)];
   flower.stroke = color;
   flower.fill = color;
   flower.linewidth = 4;
   flower.cap = 'round';
-  // flower.rotation = Math.floor(Math.random() * 4) * Math.PI / 2 + Math.PI / 4;
+  // flower.rotation = Math.floor(Math.random() * 16) * Math.PI / 8 + Math.PI / 16;
   // For animation later on
   flower.step = (Math.floor(Math.random() * 8) / 8) * Math.PI / 60;
   flower.step *= Math.random() > 0.5 ? - 1 : 1;
