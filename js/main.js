40f8ae8391b241b255d09429658c2a532c4aca17
merhoo <meredith.hoo1@gmail.com>
2021-03-14 10:25:29 -0700

    fixed menu

--- js/main.js
+++ js/main.js
@@ -129,25 +129,3 @@
 	const items = Array.from(document.querySelectorAll('.item'));
 	const init = (() => items.forEach(item => new ImgItem(item)))();
 };
-
-$(window).scroll(function() {
-  var isMobile = window.matchMedia("only screen and (max-width: 850px)").matches ||
-                  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  var isOtherMediaQuery = window.matchMedia("only screen and (min-width:1080px)").matches;
-  if(isMobile && !isOtherMediaQuery) {
-   // some code..
- } else {
-   var hT = $('#menu').offset().top,
-       hH = $('#menu').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-
-   if (wS > (hT+hH-wH)){
-     $('#footer').addClass("menu-footer");
-     $('#footer').removeClass("hero-footer");
-   } else {
-     $('#footer').addClass("hero-footer");
-     $('#footer').removeClass("menu-footer");
-   }
- }
-});
